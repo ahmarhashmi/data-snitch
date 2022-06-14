d97e92b2bd30b5be7b20ee60101895bafdd81812
Muhammad Ahmar Nadeem <ahmar.nadeem@vroozi.com>
2022-06-14 12:08:22 +0500

    created new project for vroozi hackathon

---
+++ src/main/java/com/vroozi/datastitch/DataStitchApplication.java
@@ -0,0 +1,13 @@
+package com.vroozi.datastitch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataStitchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataStitchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vroozi/datastitch/DataStitchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vroozi.datastitch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataStitchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
